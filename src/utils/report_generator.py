1cc5d56cd8f6742e97f82d2cdefd1ac26daa29e4
Harshal Sakpal <harshalsakpal21@gmail.com>
2025-08-24 12:05:14 +0530

    Delete src/utils directory

--- src/utils/report_generator.py
+++
@@ -1,7 +0,0 @@
-def generate_summary_report(data):
-    """Generate summary statistics from analysis data"""
-    return {
-        'total_devices': len(data.get('devices', {})),
-        'analysis_timestamp': data.get('timestamp', 'Unknown'),
-        'health_score': data.get('validation_summary', {}).get('overall_score', 0)
-    }
